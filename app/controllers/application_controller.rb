6ae346ec3c7b11472894ea5a07a22e2cb23451cd
Reid <rlj0713@gmail.com>
2021-04-07 12:20:46 -0400

    commit tree 0dded5b3fcaa7c28e608088aea61ef2a1b3dba19

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,3 @@
-
 require_relative '../../config/environment'
 
 class ApplicationController < Sinatra::Base
@@ -9,5 +8,53 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/' do
+    redirect to "/articles"
+  end
+ 
+  # Index
+  get '/articles' do
+    @articles = Article.all
+    erb :index
+  end
+
+  # New
+  get '/articles/new' do
+    @article = Article.new
+    erb :new
+  end
+  
+  # Create
+  post '/articles' do
+    @article = Article.create(params)
+    if @article == nil
+      redirect to "/articles/new"
+    else
+      redirect to "/articles/#{ @article.id }"
+    end
+  end
+
+  # Show
+  get '/articles/:id' do
+    @article = Article.find(params[:id])
+    erb :show
+  end
+  
+  # Edit
+  get '/articles/:id/edit' do
+    @article = Article.find(params[:id])
+    erb :edit
+  end
+
+  # Update
+  patch "/articles/:id" do
+    @article = Article.find(params[:id])
+    @article.update(params[:article])
+    redirect to "/articles/#{ @article.id }"
+  end
+
+  # Destroy
+  delete '/articles/:id' do
+    Article.destroy(params[:id])
+    redirect to '/articles'
   end
 end
